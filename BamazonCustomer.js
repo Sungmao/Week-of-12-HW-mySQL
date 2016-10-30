5deb56e29d02f98cbe1c790af9a4bf964cf06c54
Wu Sung-Mao <Allen@Wus-MacBook-Pro.local>
2016-10-30 16:21:03 -0500

    update customer function and add screen shot of demo

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -21,7 +21,7 @@ function displayProduct() {
 
         //console.log(response)
         for (var i = 0; i < response.length; i++) {
-            console.log(response[i].ItemID + " | " + response[i].ProductName + " | " + "Price: $" + " | " + response[i].Price+"Stock Qt: " + response[i].StockQuantity);
+            console.log(response[i].ItemID + " | " + response[i].ProductName + " | " + "Price: $" + response[i].Price+ " | " + "Stock Qt: " + response[i].StockQuantity);
         }
         console.log("-----------------------------------");
         customer(response);
@@ -36,15 +36,18 @@ function customer(response) {
     inquirer.prompt({
         name: "productIdChosen",
         type: "input",
-        message: "Which product you would like to buy? Please Input the Item ID. Thank you."
+        message: "Which product you would like to buy? Please enter the Item ID."
 
     }).then(function(result){
         //console.log(result)
+
+        var checkID = false;
 
         for(var i=0;i<response.length;i++){
 
             if(response[i].ItemID==result.productIdChosen){
                 console.log(response[i].ProductName)
+                checkID = true;
                 var id = i
                 var productID = response[i].ItemID
 
@@ -53,23 +56,34 @@ function customer(response) {
                     type: "input",
                     message: "How many units do you want?"
                 }).then(function(res){
-                    //console.log(id)
-                    //console.log(res)
+
+                    var totalCost = parseFloat(res.number)*parseFloat(response[id].Price)
+                    //console.log(totalCost)
                     if((response[id].StockQuantity-res.number)>0){
                         //console.log(response[id].StockQuantity)
                         newNum = response[id].StockQuantity - res.number;
                         connection.query("UPDATE products SET ? WHERE ?",[{StockQuantity: newNum}, {ItemID: productID}], function(err, response2){
                             console.log("Your Order placed!!")
+                            console.log("total cost of your purchase is "+"$"+totalCost)
+                            process.exit()
 
                         })
 
-
-
+                    } else{
+                        console.log("Insufficient quantity!")
+                        customer(response)
                     }
 
                 })
 
 
+            }
+
+            //console.log(checkID, i, response.length)
+
+            if(checkID==false && i==(response.length-1)){
+                console.log("Please enter the Item ID. Thank you.")
+                customer(response)
             }
 
         }
