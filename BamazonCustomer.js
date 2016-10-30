b3cfaaa1eaab271fa70f5c53da3d8093a1576e79
Wu Sung-Mao <Allen@Wus-MacBook-Pro.local>
2016-10-30 15:07:55 -0500

    add customer function

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer')
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,13 +12,71 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    displayProduct();
 })
 
-connection.query('SELECT * FROM Products', function(err, response) {
+function displayProduct() {
 
-    //console.log(response)
-    for (var i = 0; i < response.length; i++) {
-        console.log(response[i].ItemID + " | " + response[i].ProductName + " | " + "Price: $" +response[i].Price);
-    }
-    console.log("-----------------------------------");
-})
+    connection.query('SELECT * FROM Products', function(err, response) {
+
+        //console.log(response)
+        for (var i = 0; i < response.length; i++) {
+            console.log(response[i].ItemID + " | " + response[i].ProductName + " | " + "Price: $" + " | " + response[i].Price+"Stock Qt: " + response[i].StockQuantity);
+        }
+        console.log("-----------------------------------");
+        customer(response);
+    })
+
+
+
+}
+
+function customer(response) {
+
+    inquirer.prompt({
+        name: "productIdChosen",
+        type: "input",
+        message: "Which product you would like to buy? Please Input the Item ID. Thank you."
+
+    }).then(function(result){
+        //console.log(result)
+
+        for(var i=0;i<response.length;i++){
+
+            if(response[i].ItemID==result.productIdChosen){
+                console.log(response[i].ProductName)
+                var id = i
+                var productID = response[i].ItemID
+
+                inquirer.prompt({
+                    name: "number",
+                    type: "input",
+                    message: "How many units do you want?"
+                }).then(function(res){
+                    //console.log(id)
+                    //console.log(res)
+                    if((response[id].StockQuantity-res.number)>0){
+                        //console.log(response[id].StockQuantity)
+                        newNum = response[id].StockQuantity - res.number;
+                        connection.query("UPDATE products SET ? WHERE ?",[{StockQuantity: newNum}, {ItemID: productID}], function(err, response2){
+                            console.log("Your Order placed!!")
+
+                        })
+
+
+
+                    }
+
+                })
+
+
+            }
+
+        }
+
+
+
+    })
+
+
+}
